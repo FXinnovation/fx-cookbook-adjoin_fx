4855c6352bc69070b48f1a751357ea966c9f8ee8
Zopanix <zopanix@gmail.com>
2018-03-09 12:05:52 -0500

    Remove adjoin from recipe for the moment and starting .kitchen.yml

--- recipes/kitchen.rb
+++ recipes/kitchen.rb
@@ -6,10 +6,13 @@
 # description::Recipe for kitchen tests, do not use in production
 #
 
-adjoin_fx 'default' do
-  target_ou node['adjoin_fx']['target_ou']
-  username  node['adjoin_fx']['username']
-  domain    node['adjoin_fx']['domain']
-  password  node['adjoin_fx']['password']
-  action    :join
+# adjoin_fx 'default' do
+#   target_ou node['adjoin_fx']['target_ou']
+#   username  node['adjoin_fx']['username']
+#   domain    node['adjoin_fx']['domain']
+#   password  node['adjoin_fx']['password']
+#   action    :join
+# end
+file 'C:\\Windows\\test.txt' do
+  action :create
 end
