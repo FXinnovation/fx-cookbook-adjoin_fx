538e33f1975b66ff50dfbaea632a1921693c7189
Zopanix <zopanix@gmail.com>
2018-03-09 21:46:41 -0500

    Adding inspec tests for windows and rhel

---
+++ test/inspec/rhel/default.rb
@@ -0,0 +1,24 @@
+control 'adjoin_fx - 01' do
+  title 'The required packages should be installed'
+  packages = %w(
+    sssd
+    adcli
+    realmd
+    samba-common-tools
+    krb5-libs
+    krb5-workstation
+  )
+  packages.each do |package_name|
+    describe package(package_name) do
+      it { should be_installed}
+    end
+  end
+end
+
+control 'adjoin_fx - 02' do
+  title 'The computer should be joined to a domain'
+  describe command('realm list') do
+    its('stdout') { should match(/domain-name:/) }
+    its('stdout') { should match(/realm-name:/) }
+  end
+end
