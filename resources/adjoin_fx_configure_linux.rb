9c174f61355942dc6fb11110635bb1ccec312cd3
Zopanix <zopanix@gmail.com>
2018-04-02 14:19:10 -0400

    Adding only if plus beuatification

--- resources/adjoin_fx_configure_linux.rb
+++ resources/adjoin_fx_configure_linux.rb
@@ -30,39 +30,34 @@ action :configure do
   end
 
   # Deny login from all groups if property is set for defined realm
-  if property_is_set?(:deny_all)
-    execute 'adjoin_fx_confgiure_deny_all' do
-      command "realm deny -all --unattended --realm \"#{new_resource.domain}\""
-      user    'root'
-      action  :run
-    end
-  end
+  execute 'adjoin_fx_confgiure_deny_all' do
+    command "realm deny -all --unattended --realm \"#{new_resource.domain}\""
+    user    'root'
+    action  :run
+    only_if { new_resource.deny_all }
+  end if property_is_set?(:deny_all)
 
   # Allow login from login groups if property is set
-  if property_is_set?(:login_groups)
-    # TODO: Add removal of any group which isn't passed on
-    # For each login_groups
-    new_resource.login_groups.each do |login_group|
-      # Add the login group
-      execute "adjoin_fx_configure_login_group_#{login_group}" do
-        command "realm permit --realm \"#{new_resource.domain}\" --groups \"#{login_group.tr(' ', '_').downcase}\""
-        user    'root'
-        action  :run
-        not_if  "echo \"$(realm list | grep -Pzo \"^#{new_resource.domain}(\\s{2}.*){1,}\")\" | grep \"permitted-groups:\" | grep \"#{login_group}\""
-      end
+  # TODO: Add removal of any group which isn't passed on
+  # For each login_groups
+  new_resource.login_groups.each do |login_group|
+    # Add the login group
+    execute "adjoin_fx_configure_login_group_#{login_group}" do
+      command "realm permit --realm \"#{new_resource.domain}\" --groups \"#{login_group.tr(' ', '_').downcase}\""
+      user    'root'
+      action  :run
+      not_if  "echo \"$(realm list | grep -Pzo \"^#{new_resource.domain}(\\s{2}.*){1,}\")\" | grep \"permitted-groups:\" | grep \"#{login_group}\""
     end
-  end
+  end if property_is_set?(:login_groups)
 
   # Allow login from login users if property is set
-  if property_is_set?(:login_users)
-    # TODO: Add removal of any user which isn't passed on
-    new_resource.login_users.each do |login_user|
-      execute "adjoin_fx_configure_login_user_#{login_user}" do
-        command "realm permit --realm \"#{new_resource.domain}\" \"#{login_user.tr(' ', '_').downcase}@#{new_resource.domain}\""
-        user    'root'
-        action  :run
-        not_if  "echo \"$(realm list | grep -Pzo \"^#{new_resource.domain}(\\s{2}.*){1,}\")\" | grep \"permitted-logins:\" | grep \"#{login_user}\""
-      end
+  # TODO: Add removal of any user which isn't passed on
+  new_resource.login_users.each do |login_user|
+    execute "adjoin_fx_configure_login_user_#{login_user}" do
+      command "realm permit --realm \"#{new_resource.domain}\" \"#{login_user.tr(' ', '_').downcase}@#{new_resource.domain}\""
+      user    'root'
+      action  :run
+      not_if  "echo \"$(realm list | grep -Pzo \"^#{new_resource.domain}(\\s{2}.*){1,}\")\" | grep \"permitted-logins:\" | grep \"#{login_user}\""
     end
-  end
+  end if property_is_set?(:login_users)
 end
