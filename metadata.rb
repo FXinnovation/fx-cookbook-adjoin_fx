438b6b1f12e7240054b8b3a86d0e837f3c31d145
Zopanix <zopanix@gmail.com>
2018-05-17 05:46:58 -0400

    Bump version of cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'FXinnovation'
 maintainer_email 'cloudsquad@fxinnovation.com'
 description      'Joins a machine to a Active Directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.2.1'
 chef_version     '>= 12.14' if respond_to?(:chef_version)
 issues_url       'https://bitbucket.org/fxadmin/public-common-cookbook-adjoin_fx/issues'
 source_url       'https://bitbucket.org/fxadmin/public-common-cookbook-adjoin_fx'
